0c8680e6a2106907767e3d50589b28a94d69068f
ALi <s.arme@ya.ru>
2020-11-19 23:03:55 +0330

    Create consumer function

--- consumer.py
+++ consumer.py
@@ -1,3 +1,7 @@
+from multiprocessing.managers import BaseManager
+from multiprocessing import Process, current_process
+import time
+import random
 class QueueManager(BaseManager):
     pass
 
@@ -9,3 +13,19 @@ def cpu_work(work_units):
     x = 0
     for w in range(work_units * 1_000_000):
         x += 1
+
+
+def consumer(r_manger):
+    r_manger.connect()
+    serving_line = r_manger.get_queue()
+    while True:
+        try:
+            q = serving_line.get(timeout=10)
+        except Exception as e:
+            print(current_process().name, "exited")
+            break
+        print(current_process().name, "ate", q)
+        # sleep can represent that the thread is doing some io task
+        # to simulate cpu intensive work we can use for loops
+        time.sleep(random.random())
+        cpu_work(2)
