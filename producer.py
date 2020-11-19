2616bcafb6ba02837146387cd78b02f23310ffe2
ALi <s.arme@ya.ru>
2020-11-19 23:07:31 +0330

    Creeate producer function

--- producer.py
+++ producer.py
@@ -7,3 +7,13 @@ class QueueManager(BaseManager):
 QueueManager.register('get_queue')
 QueueManager.register('get_max')
 
+
+def producer(r_manger):
+    i = 1
+    while True:
+        print(
+            f'Served {i % size} - remaining capacity: {size - serving_line.qsize()}')
+        serving_line.put(f"Bowl #{i % size}")
+        i += 1
+        # sleep can represent that the thread is doing some io task
+        time.sleep(0.5)
