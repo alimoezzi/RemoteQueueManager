20395d5a3c8d917106a68b1f0dfe6c12b156cf03
ALi <s.arme@ya.ru>
2020-11-19 23:03:08 +0330

    Define consumer task

--- consumer.py
+++ consumer.py
@@ -4,3 +4,8 @@ class QueueManager(BaseManager):
 
 QueueManager.register('get_queue')
 QueueManager.register('get_max')
+
+def cpu_work(work_units):
+    x = 0
+    for w in range(work_units * 1_000_000):
+        x += 1
