b74d4572b27a5a3d516a46b990a1e08f871c0346
ALi <s.arme@ya.ru>
2020-11-19 22:56:23 +0330

    Start Manager server in localhost and register shared get_queue and get_max proxy

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ class QueueManager(BaseManager):
     pass
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # normally consuming  takes 0.1 longer than producing it so producer might add  a full queue!
+    # also we should notify consumer that queue is empty
+    # for cpu intensive work as of GIL there's no simultaneous threads so they can not consume as fast as producer does
+    # here we need to use multiprocessing
+    QueueManager.register('get_queue', lambda: queue)
+    QueueManager.register('get_max', lambda: {"size": queue._maxsize})
+    manager = QueueManager(address=('localhost', 50000), authkey=b'abracadabra')
+    server = manager.get_server()
+    server.serve_forever()
